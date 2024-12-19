635385ba08f8a0a0612ec7f5983e6dfdbdaea139
Nimesh Monson <nimesh.monson.96@gmail.com>
2024-12-19 12:03:34 -0800

    Initialized openai sdk

---
+++ general_react_agent/open_ai_client.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-4o",
+)
+print(chat_completion)
